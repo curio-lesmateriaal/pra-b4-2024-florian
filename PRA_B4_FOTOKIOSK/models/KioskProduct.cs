4abbd34c1a195e06aced08db5e930d7bca32223d
cruvex <woma811@gmail.com>
2024-06-02 15:08:29 +0200

    cleanup

--- PRA_B4_FOTOKIOSK/models/KioskProduct.cs
+++ PRA_B4_FOTOKIOSK/models/KioskProduct.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,7 @@ namespace PRA_B4_FOTOKIOSK.models
 {
     public class KioskProduct
     {
-
         public string Name { get; set; }
-
+        public Decimal Price { get; set; }
     }
 }
